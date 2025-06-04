85090f6d6a8185e37ab382fc44734051bccbd14f
daancs <dadi.andrason@gmail.com>
2025-06-04 16:36:30 +0200

    testing stuff

--- SPACe/steps_single_plate/_segmentation.py
+++ SPACe/steps_single_plate/_segmentation.py
@@ -98,9 +98,7 @@ class SegmentationPartI:
                 z_axis=None,
                 channel_axis=None,
                 resample=False,
-                min_size=self.args.min_sizes["w1"],
-                flow_threshold=0
-            )
+                min_size=self.args.min_sizes["w1"])
 
             w2_mask, _, _, _ = self.cellpose_model.eval(
                 img[1],
@@ -110,8 +108,7 @@ class SegmentationPartI:
                 z_axis=None,
                 channel_axis=None,
                 resample=False,
-                min_size=self.args.min_sizes["w2"],
-                flow_threshold=0)
+                min_size=self.args.min_sizes["w2"])
         # # print(f"cellpose w1 takes {time.time()-stime} seconds")
         ###########################################################################################
         elif self.args.step2_segmentation_algorithm == "w1=pycle_w2=pycle":
